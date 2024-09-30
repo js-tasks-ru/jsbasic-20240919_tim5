9b745d573765fffb522132c3a2754489b03661ba
Tim5 <timavd555@gmail.com>
2024-09-30 12:41:50 +0300

    module 2 task 1-3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
-function sumSalary(salaries) {
-  // ваш код...
+﻿function sumSalary(salaries) {
+let sum = 0;
+
+for (let k in salaries) {
+
+if (typeof salaries[k] === 'number' && isFinite(salaries[k])) {
+
+sum += salaries[k];
+
+}
+
+}
+
+return sum;
 }
