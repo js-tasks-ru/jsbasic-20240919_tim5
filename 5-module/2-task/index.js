0afaa503f71383e52e6e107501052f4cdb1585f0
TimGridy <timavd555@gmail.com>
2024-10-28 01:40:25 +0300

    module 5 task 1-3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
   // ваш код...
+const button = document.querySelector('.toggle-text-button');
+const textDiv = document.getElementById('text');
+button.addEventListener('click', function() {
+  if (textDiv.hasAttribute('hidden')) {
+    textDiv.removeAttribute('hidden'); 
+  } else {
+    textDiv.setAttribute('hidden', true); 
+  }
+});
 }
+
